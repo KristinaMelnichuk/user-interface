7da97f01c4207d17b435fe253cff224ff9bb1af6
Kristina <a.kristina.melnichuk@gmail.com>
2023-11-03 16:36:56 +0300

    the first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.zdnet.com/a/img/resize/ba1b1ab92085d777ab5e313b34c66a94b7aa1236/2023/06/05/79a43eb8-ce38-488c-8cc0-e04699aaca7f/bing.jpg?auto=webp&width=1280" />
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New posts
+        </div>
+        <div>
+          post 1
+        </div>
+        <div>
+          post 2
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Profile;
